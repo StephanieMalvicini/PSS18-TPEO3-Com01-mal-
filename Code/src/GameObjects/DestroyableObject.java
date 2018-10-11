7e3f5aacabdbf8c0672b987aa0bc5fd75ddaf2be
ManuBenito98 <manuelbb@hotmail.es>
2018-10-10 23:59:51 -0300

    It is done

---
+++ Code/src/GameObjects/DestroyableObject.java
@@ -0,0 +1,27 @@
+package GameObjects;
+
+import Collisions.Collider;
+
+public abstract class DestroyableObject extends GameObject{
+	protected int health;
+	protected Collider c;
+	
+	public int getHealth() {
+		return health;
+	}
+
+
+
+	public boolean isAlive() {
+		return health > 0;
+	}
+
+	public abstract void destroySelf(); // TODO: refactorear detruccion de entidades
+
+	public void damage(float d){
+		health -= d;
+
+	}
+
+	
+}
